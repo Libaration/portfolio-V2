a878a2f19d870932d5ce9b20e628d689e71cd16a
Cristian Deleon <libaration@gmail.com>
2021-10-24 01:10:49 -0400

    adds profile picture edits about me component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Box, Flex, Text, Heading, useColorModeValue } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Text,
+  Heading,
+  useColorModeValue,
+  Avatar,
+} from '@chakra-ui/react';
+import profilepicture from '../assets/profilepicture.jpeg';
 export default function About() {
   const lightDarkBoxBackground = useColorModeValue('#4A556890', '#4A556840');
   const lightDarkText = useColorModeValue(
@@ -7,44 +15,53 @@ export default function About() {
     'linear(to-l, #F6AD55, #81E6D9)'
   );
   return (
-    <Flex
-      mt={-1}
-      zIndex={2}
-      position="relative"
-      justifyContent="center"
-      alignItems="center"
-      flexDirection="column"
-      textAlign="center"
-    >
-      <Box
-        backgroundColor={lightDarkBoxBackground}
-        borderRadius={15}
-        w={['20rem', '20rem', '3xl']}
+    <>
+      <Flex
+        mt={-1}
+        zIndex={2}
+        position="relative"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+        textAlign="center"
       >
-        <Heading color="white" p={2}>
-          <Text bgGradient={lightDarkText} bgClip="text">
-            Hi there 👋 <br />
-            I'm Cristian Deleon, a Web Developer, Designer & Creator
-          </Text>
+        <Box
+          backgroundColor={lightDarkBoxBackground}
+          borderRadius={15}
+          w={['20rem', '20rem', '3xl']}
+        >
+          <Heading color="white" p={2}>
+            <Text bgGradient={lightDarkText} bgClip="text">
+              Hi there 👋 <br />
+              I'm a Full Stack Developer based in Virginia
+            </Text>
+          </Heading>
+        </Box>
+      </Flex>
+      <Flex justifyContent="space-between" mt={10} p={3}>
+        <Box>
+          <Heading fontSize={['xl', 'xl', '5xl', '5xl']}>
+            Cristian Deleon
+          </Heading>
+          <Text fontSize={['sm', 'md', 'lg']}>Designer/Developer/Creator</Text>
+        </Box>
+        <Box>
+          <Avatar size={'xl'} name="Cristian Deleon" src={profilepicture} />
+        </Box>
+      </Flex>
+      <Flex flexDirection="column" mr={3} ml={3} maxWidth="800px">
+        <Heading size="md" textDecoration="underline" mt={3}>
+          About
         </Heading>
-      </Box>
-
-      <Text
-        pl={['2rem', '2rem', '6em']}
-        pr={['2rem', '2rem', '6em']}
-        pt={5}
-        fontSize={['xs', 'xs', 's', 'md']}
-      >
-        I started coding in middle school using Visual Basic 6. It was mostly
-        creating hex/memory editors and packet loggers for online games in C#
-        and VB6. I've made the switch to Web Development and couldn't be
-        happier. I love Front End design and working closely with customer
-        facing UI. I graduated from Flatiron bootcamp where I really got to put
-        everything together and dive into the full stack! I've worked
-        extensively with Rails on backends for it's simplicity and opinionated
-        code, and React/Redux for it's reusability on the frontend, I'm a
-        passionate coder and pride myself in learning new concepts quickly!
-      </Text>
-    </Flex>
+        <Text fontSize="sm" mb={10}>
+          Cristian is full-stack / freelance developer based in Charlottesville
+          with a passion for programming and building applications to make his
+          life easier. He has always had a knack for all things creative and
+          technology in general. He loves solving real-life problems through
+          code. When not programming he loves photography,cinematography and
+          producing music.
+        </Text>
+      </Flex>
+    </>
   );
 }
